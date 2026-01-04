e17f3a91ca78923eb1ceb7d56934577b2b0ffa1d
WesternSpace <80211714+WesternSpace@users.noreply.github.com>
2026-01-04 01:05:55 -0500

    Fix splash screen hiding before loading bar reaches 100%.

---
+++ Modern/Patch/GameAppComponent_TransitionToMainMenu_Patch.cs
@@ -0,0 +1,15 @@
+﻿using HarmonyLib;
+using Keen.Game2;
+using Pulsar.Shared.Splash;
+
+namespace Pulsar.Modern.Patch;
+
+[HarmonyPatchCategory("Early")]
+[HarmonyPatch(typeof(GameAppComponent), "TransitionToMainMenu")]
+internal static class GameAppComponent_TransitionToMainMenu_Patch
+{
+    public static void Postfix()
+    {
+        SplashManager.Instance?.Delete();
+    }
+}
